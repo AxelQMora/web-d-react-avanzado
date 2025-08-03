1034c33e85880c0f50590058cca7e5ed3ee1f876
Axel Quintana Mora <axel.quintana.mora@outlook.com>
2025-08-02 21:16:22 -0600

    rutas con express

--- express/src/app.js
+++ express/src/app.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
+// console.log(process.env.PORT)
+// console.log(process.env.NOMBRE)
+
+const { infoPeliculas } = require('./peliculas')
+console.log(infoPeliculas)
+
 // 1. Importamos el módulo express
 const express = require('express')
 
-// 2. Creamo una aplicación de Express
+// 2. Creamos una aplicación de Express
 const app = express()
 
 // 3. Definimos el puerto que va a escuchar el servidor
@@ -9,6 +16,39 @@ const PORT = 3000
 
 app.get('/', (req, res) => {
   res.send('Hola mundo')
+})
+
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
+})
+
+// app.get('/api/peliculas/accion', (req, res) => {
+//  res.send(infoPeliculas.accion)
+// })
+
+app.get('/api/peliculas/accion/titulo/:titulo/:year', (req, res) => {
+  const { titulo, year } = req.params
+
+  const resultados = infoPeliculas.accion.filter(pelicula =>
+    pelicula.titulo === titulo && pelicula.year === parseInt(year)
+  )
+
+  if (resultados.length === 0) {
+    return res.status(400).send(`No se encontraron resultados para "${titulo}" del año ${year}`)
+  }
+
+  res.send(resultados)
+})
+
+app.get('/api/peliculas/comedia/:pais', (req, res) => {
+  const pais = req.params.pais
+  const resultados = infoPeliculas.comedia.filter(pelicula => pelicula.pais === pais)
+
+  if (req.query.ordenar === 'year') {
+    return res.send(resultados.sort((a, b) => b.year - a.year))
+  }
+
+  res.send(resultados)
 })
 
 app.listen(PORT, () => {
---
+++ express/src/peliculas.js
@@ -0,0 +1,44 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Mision imposible',
+      year: 2005,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'John Wick',
+      year: 2014,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Mad Max: Furia en el camino',
+      year: 2015,
+      pais: 'australia'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'Superbad',
+      year: 2007,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'The Mask',
+      year: 1994,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'La jaula de las locas',
+      year: 1978,
+      pais: 'francia'
+    }
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
